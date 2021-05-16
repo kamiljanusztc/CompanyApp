30a5cff27535b73021522e6e2b185828d7b0c41c
Kamil Janusz <kamiljanusztc@gmail.com>
2021-05-16 19:07:44 +0200

    Add models

--- models/department.model.js
+++ models/department.model.js
@@ -1,12 +1,7 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// new mongoose.Schema({
-//   attr: { type, required },
-//   attr: { type, required },
-//   attr: { type, required },
-//   attr: { type, required }
-// });
+const departmentSchema = new mongoose.Schema({
+  name: { type: String, required: true }
+});
 
-// const departmentSchema = new mongoose.Schema({
-//   name: { type: String, required: true }
-// });
+module.exports = mongoose.model('Department', departmentSchema);
