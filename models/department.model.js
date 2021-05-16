9d85ceba4cf7e0208db040a22a9f8592e14d1147
Kamil Janusz <kamiljanusztc@gmail.com>
2021-05-16 16:01:40 +0200

    Change routes

---
+++ models/department.model.js
@@ -0,0 +1,12 @@
+// const mongoose = require('mongoose');
+
+// new mongoose.Schema({
+//   attr: { type, required },
+//   attr: { type, required },
+//   attr: { type, required },
+//   attr: { type, required }
+// });
+
+// const departmentSchema = new mongoose.Schema({
+//   name: { type: String, required: true }
+// });
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -34,14 +34,14 @@ router.post('/products', (req, res) => {
 
 router.put('/products/:id', (req, res) => {
   const { name, client } = req.body;
-  req.db.collection('products').updateOne({ '_id': 'ObjectId(req.params.id)' }, { $set: { name: name, client: client }}, err => {
+  req.db.collection('products').updateOne({ _id: ObjectId(req.params.id) }, { $set: { name: name, client: client }}, err => {
     if(err) res.status(500).json({ message: err });
     else res.json({ message: 'OK' });
   });
 });
 
 router.delete('/products/:id', (req, res) => {
-  req.db.collection('products').deleteOne({ '_id': 'ObjectId(req.params.id)' }, err => {
+  req.db.collection('products').deleteOne({ _id: ObjectId(req.params.id) }, err => {
     if(err) res.status(500).json({ message: err });
     else res.json({ message: 'OK' });
   });
